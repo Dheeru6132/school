c9e695f5d19720420a6958cd0056a219cb2ffbf8
Dheeru6132 <102347927+Dheeru6132@users.noreply.github.com>
2022-07-07 21:06:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,28 +15,27 @@ dj = mysql.connector.connect(
 mycursor = dj.cursor()
 
 
-# @application.route('/forschool', methods=['POST', 'GET'])
-# def forschool():
-#     global data_status
+@application.route('/forschool', methods=['POST', 'GET'])
+def forschool():
+    global data_status
 
-#     if request.method == 'POST':
-#         data_status = {"responseStatus": 0, "results": ""}
-#         firstname = request.json['firstname']
-#         lastname = request.json['lastname']
-#         reason = request.json['reason']
-#         mobile = request.json['mobile']
-#         date = datetime.now()
-#         time = datetime.now()
-#         mycursor.execute("insert into temporary_school values(%s,%s,%s,%s,%s,%s)",(firstname, lastname, reason, mobile, date, time))
-#         dj.commit()
-#         #mycursor.close()
-#         data_status["responseStatus"] = 1
-#         data_status["results"] = "success"
-#         return data_status
-#     else:
-#         data_status["responseStatus"] = 0
-#         data_status["result"] = "Required fields are missing"
-#         return data_status
+    if request.method == 'POST':
+        data_status = {"responseStatus": 0, "results": ""}
+        firstname = request.json['firstname']
+        lastname = request.json['lastname']
+        reason = request.json['reason']
+        mobile = request.json['mobile']
+        datetime = datetime.now()
+        mycursor.execute("insert into for_school values(%s,%s,%s,%s,%s)",(firstname, lastname, reason, mobile, datetime))
+        dj.commit()
+        mycursor.close()
+        data_status["responseStatus"] = 1
+        data_status["results"] = "success"
+        return data_status
+    else:
+        data_status["responseStatus"] = 0
+        data_status["result"] = "Required fields are missing"
+        return data_status
 
 
 @application.route('/getschool', methods=['POST', 'GET'])
@@ -45,28 +44,23 @@ def getschool():
     global mycursor
     if request.method == 'GET':
         data_status = {"responseStatus": 0, "results": ""}
-        print("qwerty")
-        mycursor.execute("select * from sys.testing")
-        print("poiuyt")
+        mycursor.execute("select * from sys.for_school")
         ab = mycursor.fetchall()
         print(ab)
         l = [1,2]
-#         for i in ab:
-#             dic = {}
-#             #for j in i:
-#             dic["firstname"] = i[0]
-#             dic["lastname"] = i[1]
-#             dic["reason"] = i[2]
-#             dic["mobile"] = i[3]
-#             dic["date"] = i[4]
-#             ti = (datetime.min + i[5]).time() no need
-#             json_str = json.dumps({'created_at': ti}, default=str) no need
-#             dic["time"] = json_str no need
-#             l.append(dic)
-#         data_status["responseStatus"] = 1
-#         data_status["details"] = l
-#         data_status["results"] = "success"
-#         #print(data_status)
+        for i in ab:
+            dic = {}
+            #for j in i:
+            dic["firstname"] = i[0]
+            dic["lastname"] = i[1]
+            dic["reason"] = i[2]
+            dic["mobile"] = i[3]
+            dic["datetime"] = i[4]
+            l.append(dic)
+        data_status["responseStatus"] = 1
+        data_status["details"] = l
+        data_status["results"] = "success"
+        #print(data_status)
         return jsonify(l)
     else:
         data_status["responseStatus"] = 0
